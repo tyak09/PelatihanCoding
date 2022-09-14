6b6e46c0b29ac45c9158366cd5314902fb90b5de
tyak09 <113221646+tyak09@users.noreply.github.com>
2022-09-14 22:39:34 +0700

    220441100146_SeptyaDwiR_RumusBalokKubus_LabMM

---
+++ balok.py
@@ -0,0 +1,11 @@
+print ("Menghitung Luas dan Volume Balok")
+
+p = int(input("Masukkan Panjangnya = "))
+l = int(input("Masukkan Luasnya = "))
+t = int(input("Masukkan tingginya = "))
+
+luas = (2*p*l)+(2*p*t)+(2*l*t) 
+volume = p*l*t
+
+print("luas baloknya adalah = ", luas)
+print("volume baloknya adalah = ", volume)
---
+++ kubus.py
@@ -0,0 +1,10 @@
+print("Menghitung Luas dan Volume kubus")
+
+r =int(input("Masukkan Rusuknya = "))
+
+luas = 6*r*r
+volume = r*r*r
+
+print("luas kubus adalah =", luas)
+print("volume kubus adalah =", volume)
+
